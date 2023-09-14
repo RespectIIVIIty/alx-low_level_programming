76d729cc64bf79b2a8d3fba9882ba53fb0653864
RespectIIVIITY <respectokinalade@gmail.com>
2023-09-13 23:22:08 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.c"
+
+/**
+ * print - the alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
